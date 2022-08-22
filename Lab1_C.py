a7dd980ea2423fdea226a0ff1dc8b07a4a32f8a7
bschnedar <38088176+bschnedar@users.noreply.github.com>
2022-08-22 16:39:07 -0700

    Add files via upload

---
+++ Lab1_C.py
@@ -0,0 +1,15 @@
+#Brandon Schnedar 
+#25821724
+#1/7/2016
+#This program multiples gives the sum an product of 4 numbers
+
+def main():
+    print("This program gives the sum and product of 4 numbers")
+    A = eval(input(" Enter your first number:"))
+    B = eval(input(" Enter your second number:"))
+    C = eval(input(" Enter your third number:"))
+    D = eval(input(" Enter your fourth number:"))
+
+    print("the sum is: ", A+B+C+D)
+    print("the product  is: ", A*B*C*D)
+main()
